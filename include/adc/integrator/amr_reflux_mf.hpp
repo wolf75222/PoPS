f537ae02a05482adf86feea06114e4a2a0b42fdb
desp0042 <romain.despoullains@etudiant.univ-reims.fr>
2026-05-29 20:31:10 +0200

    feat : AMR multi-patch (niveau fin multi-box) + reflux coverage-aware (refonte AMR d)
    amr_step_2level_multipatch : le niveau fin est un MultiFab a N boxes. reflux
    COVERAGE-AWARE facon FluxRegister AMReX : on ne corrige une cellule grossiere voisine
    d'une box fine que si elle n'est PAS couverte par une autre box fine (interface
    fin-grossier reelle ; les interfaces fin-fin sont gerees par fill_boundary). + masque
    de couverture, mf_fill_fine_ghosts_multi (interp grossier puis halos fin-fin),
    mf_average_down_multi.
    
    test_amr_multipatch : test DECISIF -- 2 boxes fines pavant la meme region qu'1 grande
    box donnent un grossier BIT-IDENTIQUE (max|dUc|=0.00e+00). valide d'un coup le reflux
    coverage-aware (joint interne non reflue) ET les halos fin-fin. 42/42. reste :
    recursion N-niveaux multipatch, regrid berger-rigoutsos, load-balance.

--- include/adc/integrator/amr_reflux_mf.hpp
+++ include/adc/integrator/amr_reflux_mf.hpp
@@ -254,4 +254,166 @@ void amr_step_multilevel_mf(const Model& m, std::vector<AmrLevelMF>& L,
                                             Real(0), nullptr, nullptr, nullptr, nullptr);
 }
 
+// --- MULTI-PATCH (plusieurs boxes fines par niveau) ---
+// Le niveau fin est un MultiFab a N boxes. Le reflux est COVERAGE-AWARE : il ne
+// corrige une cellule grossiere adjacente a une box fine que si elle n'est PAS
+// couverte par une autre box fine (interface fin-grossier reelle ; les interfaces
+// fin-fin sont gerees par fill_boundary). C'est la logique FluxRegister d'AMReX.
+
+// ghosts fins multi-box depuis le grossier (interp espace+temps), PUIS fill_boundary
+// (fin-fin) ecrasera les ghosts couverts par une box voisine. coarse mono-box.
+inline void mf_fill_fine_ghosts_multi(MultiFab& Uf, const MultiFab& Uc_old,
+                                      const MultiFab& Uc_new, Real frac) {
+  device_fence();
+  const int nc = Uf.ncomp();
+  const ConstArray4 co = Uc_old.fab(0).const_array();
+  const ConstArray4 cn = Uc_new.fab(0).const_array();
+  for (int li = 0; li < Uf.local_size(); ++li) {
+    Array4 f = Uf.fab(li).array();
+    const Box2D v = Uf.box(li), g = Uf.fab(li).grown_box();
+    for (int j = g.lo[1]; j <= g.hi[1]; ++j)
+      for (int i = g.lo[0]; i <= g.hi[0]; ++i)
+        if (!v.contains(i, j)) {
+          const int ci = coarsen_index(i, 2), cj = coarsen_index(j, 2);
+          for (int k = 0; k < nc; ++k)
+            f(i, j, k) = (1 - frac) * co(ci, cj, k) + frac * cn(ci, cj, k);
+        }
+  }
+}
+
+// moyenne fin -> grossier sur l'empreinte de CHAQUE box fine (multi-box).
+inline void mf_average_down_multi(const MultiFab& Uf, MultiFab& Uc) {
+  const int nc = Uc.ncomp();
+  Array4 c = Uc.fab(0).array();
+  for (int li = 0; li < Uf.local_size(); ++li) {
+    const ConstArray4 f = Uf.fab(li).const_array();
+    const Box2D fb = Uf.box(li);
+    const int I0 = fb.lo[0] / 2, I1 = (fb.hi[0] - 1) / 2;
+    const int J0 = fb.lo[1] / 2, J1 = (fb.hi[1] - 1) / 2;
+    for_each_cell(Box2D{{I0, J0}, {I1, J1}}, [=] ADC_HD(int I, int J) {
+      for (int k = 0; k < nc; ++k)
+        c(I, J, k) = Real(0.25) * (f(2 * I, 2 * J, k) + f(2 * I + 1, 2 * J, k) +
+                                   f(2 * I, 2 * J + 1, k) + f(2 * I + 1, 2 * J + 1, k));
+    });
+  }
+}
+
+// Pas 2-niveaux conservatif, niveau fin MULTI-BOX. Uc : grossier mono-box (periodique).
+// Uf : MultiFab a N boxes fines (ratio 2, strictement interieures, alignees grossier).
+template <class Limiter = NoSlope, class NumericalFlux = RusanovFlux, class Model>
+void amr_step_2level_multipatch(const Model& m, MultiFab& Uc, const Box2D& dom, Real dxc,
+                                Real dyc, MultiFab& Uf, const MultiFab& auxc,
+                                const MultiFab& auxf, Real dt) {
+  const int r = 2, nc = Uc.ncomp();
+  const Real dxf = dxc / 2, dyf = dyc / 2, dtf = dt / r;
+  const int NX = dom.nx(), NY = dom.ny();
+
+  // masque de couverture : cellules grossieres couvertes par une box fine.
+  std::vector<char> cov(static_cast<std::size_t>(NX) * NY, 0);
+  for (int li = 0; li < Uf.local_size(); ++li) {
+    const Box2D fb = Uf.box(li);
+    for (int J = fb.lo[1] / 2; J <= (fb.hi[1] - 1) / 2; ++J)
+      for (int I = fb.lo[0] / 2; I <= (fb.hi[0] - 1) / 2; ++I)
+        cov[static_cast<std::size_t>(J) * NX + I] = 1;
+  }
+  auto covered = [&](int I, int J) {
+    return (I >= 0 && I < NX && J >= 0 && J < NY) ? cov[static_cast<std::size_t>(J) * NX + I] : char(0);
+  };
+
+  MultiFab Uc_old = Uc;
+  fill_boundary(Uc, dom, Periodicity{true, true});
+  MultiFab fxc(BoxArray(std::vector<Box2D>{xface_box(Uc.box(0))}), Uc.dmap(), nc, 0);
+  MultiFab fyc(BoxArray(std::vector<Box2D>{yface_box(Uc.box(0))}), Uc.dmap(), nc, 0);
+  compute_face_fluxes<Limiter, NumericalFlux>(m, Uc, auxc, fxc, fyc);
+
+  // registre par box fine : flux grossier (sans dt) sauve aux 4 faces.
+  struct Reg { int I0, I1, J0, J1; std::vector<Real> cL, cR, cB, cT, fL, fR, fB, fT; };
+  std::vector<Reg> regs(Uf.local_size());
+  {
+    device_fence();
+    const ConstArray4 FX = fxc.fab(0).const_array(), FY = fyc.fab(0).const_array();
+    for (int li = 0; li < Uf.local_size(); ++li) {
+      const Box2D fb = Uf.box(li);
+      Reg& g = regs[li];
+      g.I0 = fb.lo[0] / 2; g.I1 = (fb.hi[0] - 1) / 2;
+      g.J0 = fb.lo[1] / 2; g.J1 = (fb.hi[1] - 1) / 2;
+      const int nJ = g.J1 - g.J0 + 1, nI = g.I1 - g.I0 + 1;
+      g.cL.assign(nJ * nc, 0); g.cR.assign(nJ * nc, 0);
+      g.cB.assign(nI * nc, 0); g.cT.assign(nI * nc, 0);
+      g.fL.assign(nJ * nc, 0); g.fR.assign(nJ * nc, 0);
+      g.fB.assign(nI * nc, 0); g.fT.assign(nI * nc, 0);
+      for (int J = g.J0; J <= g.J1; ++J)
+        for (int k = 0; k < nc; ++k) {
+          g.cL[(J - g.J0) * nc + k] = FX(g.I0, J, k);
+          g.cR[(J - g.J0) * nc + k] = FX(g.I1 + 1, J, k);
+        }
+      for (int I = g.I0; I <= g.I1; ++I)
+        for (int k = 0; k < nc; ++k) {
+          g.cB[(I - g.I0) * nc + k] = FY(I, g.J0, k);
+          g.cT[(I - g.I0) * nc + k] = FY(I, g.J1 + 1, k);
+        }
+    }
+  }
+  mf_advance_faces(Uc, fxc, fyc, dxc, dyc, dt);
+
+  // flux fins multi-box (une face-box par box fine, meme dmap).
+  std::vector<Box2D> fxb, fyb;
+  for (int li = 0; li < Uf.local_size(); ++li) {
+    fxb.push_back(xface_box(Uf.box(li)));
+    fyb.push_back(yface_box(Uf.box(li)));
+  }
+  MultiFab fxf(BoxArray(std::move(fxb)), Uf.dmap(), nc, 0);
+  MultiFab fyf(BoxArray(std::move(fyb)), Uf.dmap(), nc, 0);
+  const Box2D fdom = Box2D::from_extents(2 * NX, 2 * NY);
+
+  for (int s = 0; s < r; ++s) {
+    mf_fill_fine_ghosts_multi(Uf, Uc_old, Uc, Real(s) / r);
+    fill_boundary(Uf, fdom, Periodicity{false, false});  // halos fin-fin
+    compute_face_fluxes<Limiter, NumericalFlux>(m, Uf, auxf, fxf, fyf);
+    device_fence();
+    for (int li = 0; li < Uf.local_size(); ++li) {
+      Reg& g = regs[li];
+      const ConstArray4 FX = fxf.fab(li).const_array(), FY = fyf.fab(li).const_array();
+      for (int J = g.J0; J <= g.J1; ++J)
+        for (int k = 0; k < nc; ++k) {
+          g.fL[(J - g.J0) * nc + k] +=
+              Real(0.5) * (FX(2 * g.I0, 2 * J, k) + FX(2 * g.I0, 2 * J + 1, k)) * dtf;
+          g.fR[(J - g.J0) * nc + k] += Real(0.5) *
+              (FX(2 * g.I1 + 2, 2 * J, k) + FX(2 * g.I1 + 2, 2 * J + 1, k)) * dtf;
+        }
+      for (int I = g.I0; I <= g.I1; ++I)
+        for (int k = 0; k < nc; ++k) {
+          g.fB[(I - g.I0) * nc + k] +=
+              Real(0.5) * (FY(2 * I, 2 * g.J0, k) + FY(2 * I + 1, 2 * g.J0, k)) * dtf;
+          g.fT[(I - g.I0) * nc + k] += Real(0.5) *
+              (FY(2 * I, 2 * g.J1 + 2, k) + FY(2 * I + 1, 2 * g.J1 + 2, k)) * dtf;
+        }
+    }
+    mf_advance_faces(Uf, fxf, fyf, dxf, dyf, dtf);
+  }
+
+  mf_average_down_multi(Uf, Uc);
+
+  // reflux coverage-aware : seulement aux faces fin-grossier (voisin non couvert).
+  device_fence();
+  Array4 c = Uc.fab(0).array();
+  for (int li = 0; li < Uf.local_size(); ++li) {
+    Reg& g = regs[li];
+    for (int J = g.J0; J <= g.J1; ++J)
+      for (int k = 0; k < nc; ++k) {
+        if (!covered(g.I0 - 1, J))
+          c(g.I0 - 1, J, k) -= (g.fL[(J - g.J0) * nc + k] - g.cL[(J - g.J0) * nc + k] * dt) / dxc;
+        if (!covered(g.I1 + 1, J))
+          c(g.I1 + 1, J, k) += (g.fR[(J - g.J0) * nc + k] - g.cR[(J - g.J0) * nc + k] * dt) / dxc;
+      }
+    for (int I = g.I0; I <= g.I1; ++I)
+      for (int k = 0; k < nc; ++k) {
+        if (!covered(I, g.J0 - 1))
+          c(I, g.J0 - 1, k) -= (g.fB[(I - g.I0) * nc + k] - g.cB[(I - g.I0) * nc + k] * dt) / dyc;
+        if (!covered(I, g.J1 + 1))
+          c(I, g.J1 + 1, k) += (g.fT[(I - g.I0) * nc + k] - g.cT[(I - g.I0) * nc + k] * dt) / dyc;
+      }
+  }
+}
+
 }  // namespace adc
