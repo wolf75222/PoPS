a0521729ec4f012c6286badf02c43922b3cdf0a8
desp0042 <romain.despoullains@etudiant.univ-reims.fr>
2026-05-29 19:56:21 +0200

    feat : recursion AMR N-niveaux sur MultiFab (refonte AMR c2)
    amr_step_multilevel_mf + AmrLevelMF + subcycle_level_mf : pendant MultiFab de
    amr_multilevel.hpp (sous-cyclage berger-oliger recursif + reflux a chaque interface),
    generique (Limiter/NumericalFlux/N-comp), bulk via for_each_cell (GPU).
    
    test_amr_multilevel_mf : equivalence BIT A BIT (max|dUc|=0.00e+00) a amr_step_multilevel
    (Fab2D) sur hierarchie 3 niveaux, 40 pas, diocotron + aux variable ; masse MultiFab
    conservee a 2.22e-16. 41/41. reste : multi-patch (berger-rigoutsos), cablage AmrCoupler,
    load-balance.

--- include/adc/integrator/amr_reflux_mf.hpp
+++ include/adc/integrator/amr_reflux_mf.hpp
@@ -144,4 +144,114 @@ void amr_step_2level_mf(const Model& m, MultiFab& Uc, const Box2D& dom, Real dxc
     }
 }
 
+// --- recursion N-niveaux (pendant MultiFab de amr_multilevel.hpp) ---
+
+// Un niveau de la hierarchie MultiFab. aux detenu ailleurs (pointeur). rC* = region
+// (coords de CE niveau) raffinee par l'enfant ; valable si has_fine.
+struct AmrLevelMF {
+  MultiFab U;
+  const MultiFab* aux;
+  Real dx, dy;
+  int rCI0, rCI1, rCJ0, rCJ1;
+  bool has_fine;
+};
+
+// Avance recursivement le niveau lev de dt (sous-cyclage Berger-Oliger r=2 + reflux).
+// pOld/pNew = etats parent bornant le pas ; preg* = registre du parent (flux fins de
+// CE niveau), nul si lev==0. Generique (Limiter, NumericalFlux, N comp), seam GPU.
+template <class Limiter = NoSlope, class NumericalFlux = RusanovFlux, class Model>
+void subcycle_level_mf(const Model& m, std::vector<AmrLevelMF>& L, int lev, Real dt,
+                       const Box2D& dom, const MultiFab* pOld, const MultiFab* pNew,
+                       Real frac, std::vector<Real>* pregL, std::vector<Real>* pregR,
+                       std::vector<Real>* pregB, std::vector<Real>* pregT) {
+  const int r = 2;
+  AmrLevelMF& lv = L[lev];
+  const int nc = lv.U.ncomp();
+
+  if (lev == 0)
+    fill_boundary(lv.U, dom, Periodicity{true, true});
+  else
+    mf_fill_fine_ghosts_t(lv.U, *pOld, *pNew, frac);
+
+  MultiFab fx(BoxArray(std::vector<Box2D>{xface_box(lv.U.box(0))}), lv.U.dmap(), nc, 0);
+  MultiFab fy(BoxArray(std::vector<Box2D>{yface_box(lv.U.box(0))}), lv.U.dmap(), nc, 0);
+  compute_face_fluxes<Limiter, NumericalFlux>(m, lv.U, *lv.aux, fx, fy);
+
+  if (lev > 0) {  // contribution au registre du parent (flux fins x dt)
+    device_fence();
+    const ConstArray4 FX = fx.fab(0).const_array(), FY = fy.fab(0).const_array();
+    const AmrLevelMF& par = L[lev - 1];
+    const int pI0 = par.rCI0, pI1 = par.rCI1, pJ0 = par.rCJ0, pJ1 = par.rCJ1;
+    for (int J = pJ0; J <= pJ1; ++J)
+      for (int k = 0; k < nc; ++k) {
+        (*pregL)[(J - pJ0) * nc + k] +=
+            Real(0.5) * (FX(2 * pI0, 2 * J, k) + FX(2 * pI0, 2 * J + 1, k)) * dt;
+        (*pregR)[(J - pJ0) * nc + k] += Real(0.5) *
+            (FX(2 * pI1 + 2, 2 * J, k) + FX(2 * pI1 + 2, 2 * J + 1, k)) * dt;
+      }
+    for (int I = pI0; I <= pI1; ++I)
+      for (int k = 0; k < nc; ++k) {
+        (*pregB)[(I - pI0) * nc + k] +=
+            Real(0.5) * (FY(2 * I, 2 * pJ0, k) + FY(2 * I + 1, 2 * pJ0, k)) * dt;
+        (*pregT)[(I - pI0) * nc + k] += Real(0.5) *
+            (FY(2 * I, 2 * pJ1 + 2, k) + FY(2 * I + 1, 2 * pJ1 + 2, k)) * dt;
+      }
+  }
+
+  if (!lv.has_fine) {
+    mf_advance_faces(lv.U, fx, fy, lv.dx, lv.dy, dt);  // feuille
+    return;
+  }
+
+  // niveau avec enfant : registre local + flux grossier sauve (sans dt)
+  const int cI0 = lv.rCI0, cI1 = lv.rCI1, cJ0 = lv.rCJ0, cJ1 = lv.rCJ1;
+  const int nI = cI1 - cI0 + 1, nJ = cJ1 - cJ0 + 1;
+  std::vector<Real> cL(nJ * nc), cR(nJ * nc), cB(nI * nc), cT(nI * nc);
+  {
+    device_fence();
+    const ConstArray4 FX = fx.fab(0).const_array(), FY = fy.fab(0).const_array();
+    for (int J = cJ0; J <= cJ1; ++J)
+      for (int k = 0; k < nc; ++k) {
+        cL[(J - cJ0) * nc + k] = FX(cI0, J, k);
+        cR[(J - cJ0) * nc + k] = FX(cI1 + 1, J, k);
+      }
+    for (int I = cI0; I <= cI1; ++I)
+      for (int k = 0; k < nc; ++k) {
+        cB[(I - cI0) * nc + k] = FY(I, cJ0, k);
+        cT[(I - cI0) * nc + k] = FY(I, cJ1 + 1, k);
+      }
+  }
+  std::vector<Real> fL(nJ * nc, 0), fR(nJ * nc, 0), fB(nI * nc, 0), fT(nI * nc, 0);
+
+  MultiFab U_old = lv.U;  // etat t (interp temporelle de l'enfant)
+  mf_advance_faces(lv.U, fx, fy, lv.dx, lv.dy, dt);  // lv.U -> t+dt
+
+  for (int s = 0; s < r; ++s)
+    subcycle_level_mf<Limiter, NumericalFlux>(m, L, lev + 1, dt / r, dom, &U_old,
+                                              &lv.U, Real(s) / r, &fL, &fR, &fB, &fT);
+
+  mf_average_down(L[lev + 1].U, lv.U, cI0, cI1, cJ0, cJ1);
+
+  device_fence();
+  Array4 c = lv.U.fab(0).array();
+  for (int J = cJ0; J <= cJ1; ++J)
+    for (int k = 0; k < nc; ++k) {
+      c(cI0 - 1, J, k) -= (fL[(J - cJ0) * nc + k] - cL[(J - cJ0) * nc + k] * dt) / lv.dx;
+      c(cI1 + 1, J, k) += (fR[(J - cJ0) * nc + k] - cR[(J - cJ0) * nc + k] * dt) / lv.dx;
+    }
+  for (int I = cI0; I <= cI1; ++I)
+    for (int k = 0; k < nc; ++k) {
+      c(I, cJ0 - 1, k) -= (fB[(I - cI0) * nc + k] - cB[(I - cI0) * nc + k] * dt) / lv.dy;
+      c(I, cJ1 + 1, k) += (fT[(I - cI0) * nc + k] - cT[(I - cI0) * nc + k] * dt) / lv.dy;
+    }
+}
+
+// Driver : un pas dt de la hierarchie complete (niveau 0 = grossier).
+template <class Limiter = NoSlope, class NumericalFlux = RusanovFlux, class Model>
+void amr_step_multilevel_mf(const Model& m, std::vector<AmrLevelMF>& L,
+                            const Box2D& dom, Real dt) {
+  subcycle_level_mf<Limiter, NumericalFlux>(m, L, 0, dt, dom, nullptr, nullptr,
+                                            Real(0), nullptr, nullptr, nullptr, nullptr);
+}
+
 }  // namespace adc
