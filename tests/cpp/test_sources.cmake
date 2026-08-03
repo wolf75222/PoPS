f5afa0ec8f6bf2e207c5ca002abac10f1ad30f56
desp0042 <romain.despoullains@etudiant.univ-reims.fr>
2026-08-03 22:01:49 +0200

    test(mesh): prove ND cell and face execution

--- tests/cpp/test_sources.cmake
+++ tests/cpp/test_sources.cmake
@@ -164,6 +164,7 @@ set(POPS_CPP_TEST_SOURCE_test_mpi_system_solve_fields "tests/cpp/integration/mpi
 set(POPS_CPP_TEST_SOURCE_test_multifab "tests/cpp/unit/mesh/test_multifab.cpp")
 set(POPS_CPP_TEST_SOURCE_test_nd_boundary_schedule "tests/cpp/unit/mesh/test_nd_boundary_schedule.cpp")
 set(POPS_CPP_TEST_SOURCE_test_nd_distribution "tests/cpp/unit/mesh/test_nd_distribution.cpp")
+set(POPS_CPP_TEST_SOURCE_test_nd_execution "tests/cpp/unit/mesh/test_nd_execution.cpp")
 set(POPS_CPP_TEST_SOURCE_test_nd_layout "tests/cpp/unit/mesh/test_nd_layout.cpp")
 set(POPS_CPP_TEST_SOURCE_test_nd_topology "tests/cpp/unit/mesh/test_nd_topology.cpp")
 set(POPS_CPP_TEST_SOURCE_test_nd_translation_schedule "tests/cpp/unit/mesh/test_nd_translation_schedule.cpp")
