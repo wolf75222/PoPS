10c5dfe19cca381edecd775f6017a3b8e6c6571b
desp0042 <romain.despoullains@etudiant.univ-reims.fr>
2026-09-08 23:32:31 +0200

    test(architecture): classify spatial solve context authorities

--- tests/python/architecture/test_amr_program_support_parity.py
+++ tests/python/architecture/test_amr_program_support_parity.py
@@ -31,6 +31,8 @@ CONTEXT_FRAGMENT_PATHS = frozenset(
         "pops/runtime/program/amr_program_context_spatial.inc",
         "pops/runtime/program/amr_program_context_field_runtime_public.inc",
         "pops/runtime/program/amr_program_context_diffusion.inc",
+        "pops/runtime/program/amr_program_context_spatial_implicit.inc",
+        "pops/runtime/program/amr_program_context_spatial_imex.inc",
         "pops/runtime/program/amr_program_context_flux_expression_public.inc",
         "pops/runtime/program/amr_program_context_spatial_operations.inc",
         "pops/runtime/program/amr_program_context_history_checkpoint_public.inc",
